a57a152eb4e1b44a4983f6fb3d9bc9863a9f9868
MilkaVi <nikolaycswow@mail.ru>
2020-11-14 16:18:17 +0300

    admin and user

--- src/main/java/se/config/WebSecurityConfig.java
+++ src/main/java/se/config/WebSecurityConfig.java
@@ -36,8 +36,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers( "/home").permitAll()
+                .antMatchers("/admin/**").hasRole( "ADMIN")
+                .antMatchers("/hello", "/greeting").hasRole( "USER")
                 .and()
                 .formLogin()
                 .loginPage("/login")
